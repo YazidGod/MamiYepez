8e4d942658394507c369bfe339f15ce7980c1c6e
Adrian <werrero2017x@gmail.com>
2026-03-11 18:33:07 -0600

    card y nav bar corregida

--- src/app/pagina-clases/pagina-clases.ts
+++ src/app/pagina-clases/pagina-clases.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './pagina-clases.css',
 })
 export class PaginaClases {
-
+  clases = [
+    { nombre: 'Clases de Repostería', descripcion: 'Aprende a hacer pasteles, galletas y más.', dia: 'Lunes y Miércoles' ,hora: '4-7', img: 'img/clases-reposteria.jpg'},
+    { nombre: 'Clases de Decoración', descripcion: 'Domina el arte de decorar tus creaciones.', dia: 'Martes y Jueves' ,hora: '4-7', img: 'img/clases-decoracion.jpg'},
+    { nombre: 'Clases de Panadería', descripcion: 'Aprende a hacer pan fresco y delicioso.', dia: 'Viernes' ,hora: '4-7', img: 'img/clases-panaderia.jpg'},
+  ];
 }
--- src/app/pagina-de-precentacion/pagina-de-precentacion.ts
+++ src/app/pagina-de-precentacion/pagina-de-precentacion.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './pagina-de-precentacion.css',
 })
 export class PaginaDePrecentacion {
-
+  claseDestacada = [
+    { titulo: "Domina el Arte de la Repostería.", subtitulo: "De Cero a Experto.", descripcion: "¿Siempre has soñado con hornear el pastel perfecto? Nuestro curso integral está diseñado tanto para principiantes como para entusiastas que desean perfeccionar su técnica. Lo que aprenderás: Manejo de batidos, secretos de horneado, elaboración de cremas y técnicas avanzadas de decoración con fondant y buttercream.", boton: "Ver curso" }
+  ]
 }
